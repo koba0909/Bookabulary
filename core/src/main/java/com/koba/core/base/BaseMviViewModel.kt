112801ab98c836c37ac2586c8b5f4436c056a0b0
koba <koba1mobile@gmail.com>
2022-09-06 15:33:53 +0900

    [#5] BaseMviViewModel에 mvi기본 요소 추가 및 state, effect flow 작성

--- core/src/main/java/com/koba/core/base/BaseMviViewModel.kt
+++ core/src/main/java/com/koba/core/base/BaseMviViewModel.kt
@@ -1,7 +1,21 @@
 package com.koba.core.base
 
 import androidx.lifecycle.ViewModel
+import com.koba.core.mvi.MviEffect
+import com.koba.core.mvi.MviIntent
+import com.koba.core.mvi.MviSideEffect
+import com.koba.core.mvi.MviState
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class BaseMviViewModel : ViewModel() {
+class BaseMviViewModel<I : MviIntent, S : MviState, E : MviEffect, SE : MviSideEffect>(
+    initialState: S
+) : ViewModel() {
+    private val _state = MutableStateFlow(initialState)
+    val state = _state.asStateFlow()
 
+    private val _effect = MutableSharedFlow<E>()
+    val effect = _effect.asSharedFlow()
 }
