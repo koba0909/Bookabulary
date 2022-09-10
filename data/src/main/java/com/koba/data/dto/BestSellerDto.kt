a41d44606017860b63ea58ff62d77d3182cedb92
koba <koba1mobile@gmail.com>
2022-09-10 16:21:24 +0900

    [#10] 베스트 셀러 파싱을 위한 dto 작성

---
+++ data/src/main/java/com/koba/data/dto/BestSellerDto.kt
@@ -0,0 +1,33 @@
+package com.koba.data.dto
+
+import androidx.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * 추후 필요한 항목이 있을시 추가 예정
+ */
+
+@Keep
+@Serializable
+data class BestSellerDto(
+    val items: List<ItemDetail>
+)
+
+@Keep
+@Serializable
+data class ItemDetail(
+    val title: String,
+    val description: String,
+    val pubDate: String,
+    val priceStandard: Int,
+    @SerialName("coverLargeUrl")
+    val imageUrl: String,
+    val categoryName: String,
+    @SerialName("customerReviewRank")
+    val reviewRating: Float,
+    val author: String,
+    val rank: Int,
+    val publisher: String,
+    val link: String
+)
