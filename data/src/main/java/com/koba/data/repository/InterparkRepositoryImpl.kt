39eb5aea09600c1e95d997f1a9586198d996f090
koba <koba1mobile@gmail.com>
2022-09-27 14:38:04 +0900

    [#8] hilt 적용 후 빌드 이슈 수정

--- data/src/main/java/com/koba/data/repository/InterparkRepositoryImpl.kt
+++ data/src/main/java/com/koba/data/repository/InterparkRepositoryImpl.kt
@@ -4,8 +4,9 @@ import com.koba.data.service.InterparkService
 import com.koba.data.toBooks
 import com.koba.domain.model.Book
 import com.koba.domain.repository.InterparkRepository
+import javax.inject.Inject
 
-class InterparkRepositoryImpl(
+class InterparkRepositoryImpl @Inject constructor(
     private val service: InterparkService
 ) : InterparkRepository {
     override suspend fun getBestSeller(): List<Book> {
