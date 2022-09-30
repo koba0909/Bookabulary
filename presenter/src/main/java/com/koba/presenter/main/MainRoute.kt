55841e2f0b32c5a17c84135183c17f1eeaffe9b7
koba0909 <koba1mobile@gmail.com>
2022-09-30 15:49:47 +0900

    [#15] ViewModel 대신 구성요소를 넘기도록 수정, ViewModel에 Intent를 전달하기 위해 접근제어자 수정

---
+++ presenter/src/main/java/com/koba/presenter/main/MainRoute.kt
@@ -0,0 +1,17 @@
+package com.koba.presenter.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+
+@Composable
+fun MainRoute(viewModel: MainViewModel) {
+    val state = viewModel.state.collectAsState()
+
+    MainScreen(
+        state = state,
+        effectFlow = viewModel.effect,
+        onRequestRefreshBookList = {
+            viewModel.handleIntent(MainIntent.RequestBestSellerList)
+        }
+    )
+}
