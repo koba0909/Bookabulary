7aa504b357ce2556893ac90d29baaea5366e221d
koba0909 <koba1mobile@gmail.com>
2022-10-09 16:05:21 +0900

    [#12] 추천, 신간 도서를 불러오기 위한 intent, sideEffect 처리 추가

--- presenter/src/main/java/com/koba/presenter/main/MainViewModel.kt
+++ presenter/src/main/java/com/koba/presenter/main/MainViewModel.kt
@@ -3,13 +3,17 @@ package com.koba.presenter.main
 import androidx.lifecycle.viewModelScope
 import com.koba.core.base.BaseMviViewModel
 import com.koba.domain.usecase.GetBestSellerUseCase
+import com.koba.domain.usecase.GetNewBooksUseCase
+import com.koba.domain.usecase.GetRecommendBooksUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getBestSellerUseCase: GetBestSellerUseCase
+    private val getBestSellerUseCase: GetBestSellerUseCase,
+    private val getRecommendBooksUseCase: GetRecommendBooksUseCase,
+    private val getNewBooksUseCase: GetNewBooksUseCase
 ) : BaseMviViewModel<MainIntent, MainState, MainEffect, MainSideEffect>(MainState()) {
     init {
         handleIntent(
@@ -52,6 +56,68 @@ class MainViewModel @Inject constructor(
                     )
                 )
             }
+            is MainIntent.RequestNewBooks -> {
+                updateState {
+                    it.copy(
+                        isLoadingRecommend = true
+                    )
+                }
+
+                handleSideEffect(
+                    MainSideEffect.GetRecommendBooks
+                )
+            }
+            is MainIntent.GetRecommendBooksSuccess -> {
+                updateState {
+                    it.copy(
+                        isLoadingRecommend = false,
+                        recommendBooks = intent.books
+                    )
+                }
+            }
+            is MainIntent.GetRecommendBooksFail -> {
+                updateState {
+                    it.copy(
+                        isLoadingBestSeller = false
+                    )
+                }
+                emitEffect(
+                    MainEffect.ShowToast(
+                        "Fail to get recommend book list, cause : ${intent.throwable.message}"
+                    )
+                )
+            }
+
+            is MainIntent.RequestNewBooks -> {
+                updateState {
+                    it.copy(
+                        isLoadingNew = true
+                    )
+                }
+                handleSideEffect(
+                    MainSideEffect.GetNewBooks
+                )
+            }
+            is MainIntent.GetNewBooksSuccess -> {
+                updateState {
+                    it.copy(
+                        isLoadingNew = false,
+                        newBooks = intent.books
+                    )
+                }
+            }
+            is MainIntent.GetNewBooksFail -> {
+                updateState {
+                    it.copy(
+                        isLoadingNew = false
+                    )
+                }
+                emitEffect(
+                    MainEffect.ShowToast(
+                        "Fail to get new book list, cause : ${intent.throwable.message}"
+                    )
+                )
+            }
         }
     }
 
@@ -68,6 +134,36 @@ class MainViewModel @Inject constructor(
                     }.onFailure {
                         handleIntent(
                             MainIntent.GetBestSellerBooksFail(it)
+                        )
+                    }
+                }
+            }
+            is MainSideEffect.GetRecommendBooks -> {
+                viewModelScope.launch {
+                    runCatching {
+                        getRecommendBooksUseCase()
+                    }.onSuccess {
+                        handleIntent(
+                            MainIntent.GetRecommendBooksSuccess(it)
+                        )
+                    }.onFailure {
+                        handleIntent(
+                            MainIntent.GetNewBooksFail(it)
+                        )
+                    }
+                }
+            }
+            is MainSideEffect.GetNewBooks -> {
+                viewModelScope.launch {
+                    runCatching {
+                        getNewBooksUseCase()
+                    }.onSuccess {
+                        handleIntent(
+                            MainIntent.GetNewBooksSuccess(it)
+                        )
+                    }.onFailure {
+                        handleIntent(
+                            MainIntent.GetNewBooksFail(it)
                         )
                     }
                 }
