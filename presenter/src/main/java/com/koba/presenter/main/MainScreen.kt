d51698358655d802ef2c26b8b3e0ce4934f801d4
koba0909 <koba1mobile@gmail.com>
2022-10-10 23:40:31 +0900

    [#12] CircularProgressBar대신 SwipeRefresh 로딩 ui 사용하도록 수정

--- presenter/src/main/java/com/koba/presenter/main/MainScreen.kt
+++ presenter/src/main/java/com/koba/presenter/main/MainScreen.kt
@@ -5,18 +5,15 @@ import android.content.res.Configuration
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.TabRowDefaults
@@ -25,7 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -199,30 +195,24 @@ fun BookHorizontalPager(
 
 @Composable
 fun BookSwipeGrid(
+    modifier: Modifier = Modifier.fillMaxSize(),
     context: Context,
     books: List<Book>,
     isLoading: Boolean,
     cellCount: Int,
     onSwipeRefresh: () -> Unit
 ) {
-    Box {
-        CircularProgress(
-            modifier = Modifier.align(Alignment.Center)
-                .size(50.dp),
-            isShow = isLoading
-        )
-
-        SwipeRefresh(
-            state = rememberSwipeRefreshState(isRefreshing = isLoading),
-            onRefresh = onSwipeRefresh
+    SwipeRefresh(
+        modifier = modifier,
+        state = rememberSwipeRefreshState(isRefreshing = isLoading),
+        onRefresh = onSwipeRefresh
+    ) {
+        BookGrid(
+            books = books,
+            cellCount = cellCount
         ) {
-            BookGrid(
-                books = books,
-                cellCount = cellCount
-            ) {
-                // TODO move detail screen
-                Toast.makeText(context, it.title, Toast.LENGTH_SHORT).show()
-            }
+            // TODO move detail screen
+            Toast.makeText(context, it.title, Toast.LENGTH_SHORT).show()
         }
     }
 }
@@ -265,14 +255,5 @@ fun BookItem(book: Book, onClickBook: (Book) -> Unit) {
                 contentDescription = book.title
             )
         }
-    }
-}
-
-@Composable
-fun CircularProgress(modifier: Modifier, isShow: Boolean) {
-    if (isShow) {
-        CircularProgressIndicator(
-            modifier = modifier
-        )
     }
 }
