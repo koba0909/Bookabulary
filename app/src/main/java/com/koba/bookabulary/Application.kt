0a7b404f172ffbb1d8689bd89d5dd0bbd88013b0
koba0909 <61863078+koba0909@users.noreply.github.com>
2022-10-11 21:36:48 +0900

    Revert "[#8] hilt application 및 AndroidEntryPoint 세팅"
    This reverts commit d9c7000decad4fe3a284210b6e6d42995b013669.

--- app/src/main/java/com/koba/bookabulary/Application.kt
+++
@@ -1,8 +0,0 @@
-package com.koba.bookabulary
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
-
-@HiltAndroidApp
-class Application @Inject constructor() : Application()
