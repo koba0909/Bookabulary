0c93314d67586f128f8f72cab6bc926c0f051d73
koba0909 <koba1mobile@gmail.com>
2022-10-25 19:34:29 +0900

    [#23] 상세 화면 intent 및 state 정의

---
+++ domain/src/main/java/com/koba/domain/model/YoutubeSearchResult.kt
@@ -0,0 +1,7 @@
+package com.koba.domain.model
+
+class YoutubeSearchResult (
+    val thumbnailUrl: String,
+    val title: String,
+    val youtuber: String
+)
--- presenter/src/main/java/com/koba/presenter/detail/DetailElements.kt
+++ presenter/src/main/java/com/koba/presenter/detail/DetailElements.kt
@@ -1,2 +1,19 @@
 package com.koba.presenter.detail
 
+import com.koba.domain.model.Book
+import com.koba.domain.model.YoutubeSearchResult
+
+data class DetailState(
+    val book: Book,
+    val youtubeSearchResult: YoutubeSearchResult?
+)
+
+sealed interface DetailIntent {
+    data class ShowDetailScreen(val book: Book)
+
+    data class RequestYoutubeSearch(val keyword: String)
+
+    data class SuccessYoutubeSearch(val result: YoutubeSearchResult)
+
+    data class FailYoutubeSearch(val exception: Exception)
+}
